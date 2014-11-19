97e651431382eae595815b7bf5c9f30251329095
henryyou <henryyou@fb.com>
2014-11-19 11:42:37 -0800

    Add some more words to the sample list

--- VocabLearn/VocabListStore.m
+++ VocabLearn/VocabListStore.m
@@ -27,6 +27,14 @@
                                                  @"horizon": @"(n) the apparent line connecting the earth and the sky",
                                                  @"host": @"(n) entertainer, innkeeper, an army, multitude",
                                                  @"blur": @"(v) make indistinct, black-out writing",
+                                                 @"fad": @"(n) fashion, interest, enthusiasm, preference, etc…that is not likely to last",
+                                                 @"fade": @"(v) Lose color, freshness or vigor",
+                                                 @"cattle": @"(n) animals with horns and cloven hoofs auch as cows, bulls and bullocks",
+                                                 @"kettle": @"(n) container with a spout, lid and handle, used for boiling water or milk",
+                                                 @"cede": @"(v) give up one's rights to or possession of something",
+                                                 @"seed": @"(n) part of a plant from which a new plant of the same kind can grow",
+                                                 @"click": @"(n) short sharp sound like that of a key turning in a lock",
+                                                 @"clique": @"(n) small group of people often with shared interets who associate closely and exclude others from their group"
                                                  }];
             
             instance = [[VocabListStore alloc] init];
